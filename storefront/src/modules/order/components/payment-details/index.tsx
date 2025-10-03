8932017b7a018b467071a63427a0e7844f82fb3f
Azim Jiwani <azimjiwani4@gmail.com>
2025-10-03 11:20:06 -0400

    ui fixes

--- storefront/src/modules/order/components/payment-details/index.tsx
+++ storefront/src/modules/order/components/payment-details/index.tsx
@@ -38,26 +38,6 @@ const PaymentDetails = ({ order, paymentMode }: any) => {
                 {paymentLabels[paymentMode] ?? paymentMode}
               </Text>
             </div>
-            <div className="flex flex-col w-2/3">
-              <Text className="txt-medium-plus text-ui-fg-base mb-1">
-                Payment details
-              </Text>
-              <div className="flex gap-2 txt-medium text-ui-fg-subtle items-center">
-                <Container className="flex items-center h-7 w-fit p-2 bg-ui-button-neutral-hover">
-                  {paymentInfoMap[payment.provider_id].icon}
-                </Container>
-                <Text data-testid="payment-amount">
-                  {isStripe(payment.provider_id) && payment.data?.card_last4
-                    ? `**** **** **** ${payment.data.card_last4}`
-                    : `${convertToLocale({
-                        amount: payment.amount,
-                        currency_code: order.currency_code,
-                      })} paid at ${new Date(
-                        payment.created_at ?? ""
-                      ).toLocaleString()}`}
-                </Text>
-              </div>
-            </div>
           </div>
         )}
       </div>
